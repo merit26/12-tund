96f3a329588169d57de570a14a052286827750c3
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-11-23 21:26:14 +0200

    my work

---
+++ login.php
@@ -0,0 +1,74 @@
+
+<?php require_once("header.php");?>
+
+<!Menüü>
+
+<nav class="navbar navbar-inverse"> 
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<br> <br> <br> <br>
+ <!Sisu>
+
+<div class="container-fluid"> 
+     <div class="row"> 
+<div class="col-md-4 col-md-offset-8 col-xs-12">
+
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  </div>
+  <button type="submit" class="btn btn-default pull-right">Submit</button>
+</form>
+</div>
+	 </div>
+<div class="container-fluid"> 
+
+     <div class="row"> 
+<div class="col-md-4 col-md-offset-8 col-xs-12">
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  </div>
+  <button type="submit" class="btn btn-default pull-right">Submit</button>
+</form>
+</div>
+	 </div>
+	 </div>
+	 
+	 
+	 
+    <h1>Hello, world!</h1>
+<?php require_once("footer.php");?>
+    
